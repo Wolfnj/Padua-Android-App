f50d2268024b606bc1dae357264c4d530f239f35
Nathan Wolf <wolfnj@etsu.edu>
2019-10-11 14:30:49 -0400

    Added buttons and text fields to main activity.
    The buttons and text fields do not do anything at the moment.

--- app/src/main/java/com/example/padua/LoginActivity.java
+++ app/src/main/java/com/example/padua/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.padua;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,4 +12,25 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+
+    public void registerADevice(View view){
+
+        Intent startRegisterDeviceActivity = new Intent(LoginActivity.this, RegisterDeviceActivity.class);
+
+        startActivity(startRegisterDeviceActivity);
+
+    }
+
+
+
+    public void myDevices(View view){
+
+        Intent startMyDevicesActivity = new Intent(LoginActivity.this, DevicesActivity.class);
+
+        startActivity(startMyDevicesActivity);
+
+    }
+
+
 }
